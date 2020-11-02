7840b5591e2499997820faab4d3e72b29533756e
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2020-11-02 07:13:20 +0530

    Add files via upload

---
+++ modules/log-management/logmgmt.tf
@@ -0,0 +1,28 @@
+# Bucket Creation to store CFT YAML source code
+
+resource "aws_s3_bucket" "hitech_buck" {
+  bucket = "hitech-log-mgmt"
+  acl    = "private"
+}
+
+
+# Adding CFT YAML source code into Above created S3 Bucket
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.hitech_buck.id
+  key    = "log_management.yml"
+  source = "/var/lib/jenkins/workspace/Jenkins-Terrafrom-CFT-pipeline/modules/CFT/log_management.yml"
+
+}
+
+# CLOUDFORMATION CREATION 
+
+resource "aws_cloudformation_stack" "log_management" {
+  depends_on = [aws_s3_bucket_object.object]
+  name = "CFT-LOG-MANAGEMENT"
+  disable_rollback = true
+  parameters = {
+    OperatorEmail = var.OperatorEmail
+    }
+ template_url = "https://${aws_s3_bucket.hitech_buck.id}.s3-us-west-2.amazonaws.com/log_management.yml"
+}
