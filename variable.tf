4b04bd60fc38511e90cd6f2caf9e4259d14b2fdf
hariharanjenkin <36661965+hariharanjenkin@users.noreply.github.com>
2020-11-02 08:23:41 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,3 +1,48 @@
 variable "OperatorEmail" {
 	default = "hitechemail@gamil.com"
 }
+
+variable "PermissionsBoundary" {
+	default = ""
+}
+
+
+variable "AllowUsersToChangePassword"{
+	default = "true"
+}
+
+variable "HardExpiry"{
+	default = "false"
+}
+
+variable "MaxPasswordAge"{
+	default = "90"
+}
+ 
+variable "MinimumPasswordLength"{
+	default = "14"
+}
+
+variable "PasswordReusePrevention"{
+	default = "24"
+}
+ 
+variable "RequireLowercaseCharacters"{
+	default = "true"
+}
+
+variable "RequireNumbers"{
+	default = "true"
+}
+ 
+variable "RequireSymbols"{
+	default = "true"
+}
+  
+variable "RequireUppercaseCharacters"{
+	default = "true"
+}
+ 
+variable "LogsRetentionInDays"{
+	default = "14"
+}
